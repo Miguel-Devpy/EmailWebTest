62be777eae1caa011bb70aea236672f79f540a79
Miguel Angel <mampuerosuarez@gmail.com>
2020-07-30 13:01:52 -0500

    Update correos.php

--- correos.php
+++ correos.php
@@ -7,11 +7,9 @@
     $email = $_POST['email'];
 
     $header = "enviado desde la pagina web";
-    $mensajeCompleto = $mensaje . "\nAtentamente: " . $nombre
+    $mensajeCompleto = $mensaje . "\nAtentamente: " . $nombre;
 
     mail($destinatario,$asunto,$mensajeCompleto,$header);
     echo "<script>alert('correo enviado exitosamente')</script>";
     echo "<script> setTimeout(\"location.href='index.html'\",1000)</script>";
-
-
 ?>

b8fa8d5f46fb24212803d1994151b36c6d907ef0
Miguel Angel <mampuerosuarez@gmail.com>
2020-07-30 12:35:06 -0500

    Add files via upload

---
+++ correos.php
@@ -0,0 +1,17 @@
+<?php
+    $destinatario = 'mampueros@uni.pe';
+    // esto es al correo al que enviara el mensaje
+    $nombre = $_POST['nombre'];
+    $asunto = $_POST['asunto'];
+    $mensaje = $_POST['mensaje'];
+    $email = $_POST['email'];
+
+    $header = "enviado desde la pagina web";
+    $mensajeCompleto = $mensaje . "\nAtentamente: " . $nombre
+
+    mail($destinatario,$asunto,$mensajeCompleto,$header);
+    echo "<script>alert('correo enviado exitosamente')</script>";
+    echo "<script> setTimeout(\"location.href='index.html'\",1000)</script>";
+
+
+?>
